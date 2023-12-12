9f0d68b3eec9d7210a8c0fc84bfc2139a0e2b0e0
davidotieno <davidotieno473@gmail.com>
2023-12-12 14:53:09 +0300

    complete assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,12 @@
-# your code goes here!
+seconds_to_midnight=5
+def countdown(seconds_to_midnight):
+   while seconds_to_midnight > 0:
+    print(f"{seconds_to_midnight} SECOND(S)!")
+    seconds_to_midnight -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(seconds_to_midnight):
+  while seconds_to_midnight > 0:
+    print(f"{seconds_to_midnight} SECOND(S)!")
+    seconds_to_midnight -= 1
+    print("HAPPY NEW YEAR!")
